534b895bec2e068a1e40a9f6e35af2bdf87dada8
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-05-21 18:06:03 +0100

    :star: added fizzbuzz challenge

---
+++ src/com/tochijava/fizzbuzz.java
@@ -0,0 +1,22 @@
+package com.tochijava;
+
+import java.util.Scanner;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int number = Integer.parseInt(input.nextLine());
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
+    }
+}
